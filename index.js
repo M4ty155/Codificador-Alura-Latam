5df0d4cdacdc002408282b6d03cec0465ecdfe35
M4ty155 <matipollo98@gmail.com>
2023-04-24 15:55:38 -0300

    retoques en la funcion encriptar

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function encriptar() {
-    let texto = document.getElementById("texto").value;
+    let texto = document.getElementById("texto");
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById ("parrafo");
     let muñeco = document.getElementById ("mensaje-no-encontrado");
 
-    let textoCifrado = texto    
+    let textoCifrado = texto.value    
     .replace(/a/gi, "enter")
     .replace(/a/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
 
-    if (texto.length != 0){
-    document.getElementById("texto").value = textoCifrado;
+    if (texto.value.length != 0){
+    texto.value = textoCifrado;
     console.log(texto);
     tituloMensaje.textContent = "Texto encriptado con exito!";
     parrafo.textContent = "";
